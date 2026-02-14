a20580ea169c00fa6d4e18786cf972ad06e26a4c
Maddie <soymagdafuentes@gmail.com>
2026-02-14 09:00:13 +0100

    preview form

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -9,19 +9,19 @@ const Preview = ({ formData }) => {
           <div className="card__author">
             <div className="card__authorPhoto"></div>
             <p className="card__job">
-              Full stack Developer
+              {formData.job || "Full stack Developer"}
             </p>
-            <h3 className="card__name">Emmelie Bjôrklund</h3>
+            <h3 className="card__name"> {formData.autor || "Emmelie Bjôrklund"}</h3>
           </div>
       
           <div className="card__project">            
             <h3 className="card__name">{formData.name || "Elegant Workspace"}</h3>
-            <p className="card__slogan">Diseños Exclusivos</p>
+            <p className="card__slogan">{formData.slogan || "Diseños Exclusivos"}</p>
             <h3 className="card__descriptionTitle">Product description</h3>
-            <p className="card__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione</p>
+            <p className="card__description">{formData.desc || "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione"}</p>
 
             <div className="card__technicalInfo">
-              <p className="card__technologies">React JS - HTML - CSS</p>
+              <p className="card__technologies">{formData.technologies || "React JS - HTML - CSS"}</p>
           
               <a className="icon icon__www" href="#" title="Haz click para ver el proyecto online">
                 Web link
