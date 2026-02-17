f7596d9f929e22e3937eb1af0be16df008f677c9
YisethGarcia7 <yiseth.garcia7@gmail.com>
2026-02-17 20:24:08 +0100

    merge done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-<<<<<<< HEAD
-import { useState, useEffect } from 'react';
-import { Routes, Route} from "react-router-dom";
-=======
 
 //import React from 'react';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from "react-router-dom";
->>>>>>> main
 
 import GetAvatar from './components/GetAvatar/GetAvatar';
 import Profile from './components/Profile/Profile';
 import './main.scss';
 import { saveInStorage, loadFromStorage } from "./services/LocalStorage";
 
-<<<<<<< HEAD
-=======
-//componentes
->>>>>>> main
 import Form from './components/Form/Form';
 import Hero from './components/Hero/Hero';
 import Preview from './components/Preview/Preview';
@@ -53,7 +44,6 @@ function App() {
     saveInStorage(formData);
   }, [formData]);
 
-<<<<<<< HEAD
   const resetForm = () => {
     const emptyData = {
       name: "",
@@ -73,77 +63,50 @@ function App() {
   };
 
 
-  return (
-      <Routes>
+ return (
+  <div>
+    <Routes>
 
-        {/* Landing */}
-        <Route path="/" element={<Landing />} />
+      {/* Landing Page*/}
+      <Route
+        path="/"
+        element={<Landing />}
+      />
 
-        {/* Formulario */}
-        <Route path="/form" element={
+      {/* Página actual con formulario */}
+      <Route
+        path="/form"
+        element={
+          <div>
+            <GetAvatar avatar={avatar} updateAvatar={updateAvatar} />
+            <Profile avatar={avatar} />
+            <div className='container'>
+              <main className='main'>
+                <Header />
+                <Hero />
+                <Preview formData={formData} avatar={avatar} />
+                <Form
+                  formData={formData}
+                  setFormData={setFormData}
+                  resetForm={resetForm}
+                />
+              </main>
+            </div>
+          </div>
+        }
+      />
 
-        <>
-              <GetAvatar avatar={avatar} updateAvatar={updateAvatar} />
+      {/* Página cardUrl */}
+      <Route
+        path="/cardPage"
+        element={<CardPage formData={formData} />}
+      />
 
-              <div className='container'>
-                <main className='main'>
+    </Routes>
+    <Footer />
+  </div>
+);
 
-                  <Header/>
-                  <Hero/>
-                  <Preview formData={formData} avatar={avatar}></Preview>
-                  <Form formData={formData} setFormData={setFormData} resetForm={resetForm}></Form>
-                  <Footer/>
-                </main>
-              </div>
-              </>
-            }
-            />
-
-      </Routes>
-=======
-  return (
-    <div>
-      <Routes>
-
-          {/* Landing Page*/}
-          <Route 
-            path="/" 
-            element={<Landing />} 
-          />
-
-          {/* Página actual con formulario */}
-          <Route 
-            path="/form" 
-            element={
-              <div>
-                <GetAvatar avatar={avatar} updateAvatar={updateAvatar} />
-                <Profile avatar={avatar} />
-                <div className='container'>
-                  <main className='main'>
-                    <Header />
-                    <Hero />
-                    <Preview formData={formData}></Preview>
-                    <Form formData={formData} setFormData={setFormData}></Form>
-                  </main>
-                </div>
-              </div>
-            } 
-          />
-          {/* Página cardUrl */}
-          <Route 
-            path="/cardPage" 
-            element={<CardPage formData={formData} />}  
-          />
-      </Routes>
-      <Footer></Footer>
-    </div>
-
-  );
-}
-
->>>>>>> main
-
-  );
 }
 
 export default App;
